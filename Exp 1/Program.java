426c9e4d42f111d27d29801db54c89619188d010
akashbalte36 <36889666+akashbalte36@users.noreply.github.com>
2018-11-13 15:37:32 +0530

    Program

--- Exp 1/Program.java
+++ Exp 1/Program.java
@@ -1,1 +1,28 @@
 
+import java.util.Scanner;
+public class calculator
+{
+	public static void main(String args[])
+	{
+	int num1,num2,add,sub,mul;
+	float div;
+	Scanner s=new Scanner(System.in);
+	System.out.println("\nEnter the first number");
+	num1=s.nextInt();
+	System.out.println("\nEnter the second number");
+	num2=s.nextInt();
+	add=num1+num2;
+	System.out.println("\nThe addition of 2 numbers are:"+add);
+	sub=num1-num2;
+	System.out.println("\nThe sutraction of 2 numbers are:"+sub);
+	mul=num1*num2;
+	System.out.println("\nThe multiplication of 2 nummbers are:"+mul);
+	div=num1/num2;
+	System.out.println("\nThe Division of 2 numbers are:"+div);
+		
+		
+	}
+	
+	
+	
+}
